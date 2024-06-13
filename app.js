dfb5466289c4e91998140bf8594af0fbf943c983
fakhirana <151917341+fakhirana@users.noreply.github.com>
2024-06-13 07:53:37 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 const userRoutes = require('./routes/userRoute');
+const photoRoutes = require('./routes/photoRoutes');
 
 app.use(express.json()); //Middleware to parse JSON bodies
-app.use('/user', userRoutes);
+app.use('/users', userRoutes);
+app.use('/photos', photoRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
